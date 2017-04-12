ad52dc41923f393af4cda82694669b719877aa88
Dorian LENET <dorian.le-net@ensimag.grenoble-inp.fr>
2017-04-12 10:11:06 +0200

    mais oui bagou

---
+++ src/main/java/controleur/Controleur.java
@@ -0,0 +1,64 @@
+package controleur;
+
+
+import java.io.*;
+import javax.annotation.Resource;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.sql.DataSource;
+
+/**
+ * Le contrôleur de l'application.
+ */
+@WebServlet(name = "Controleur", urlPatterns = {"/controleur"})
+public class Controleur extends HttpServlet {
+
+    @Resource(name = "jdbc/Paiement_restaurant")
+    private DataSource ds;
+
+    /* pages d’erreurs */
+    private void invalidParameters(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/controleurErreur.jsp").forward(request, response);
+    }
+
+
+    /**
+     * Actions possibles en GET : afficher (correspond à l’absence du param),
+     * getOuvrage.
+     */
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+
+        if (action == null) {
+            request.getRequestDispatcher("/WEB-INF/finRepas.jsp").forward(request, response);
+        } else if (action.equals("paiement")) {
+            request.getRequestDispatcher("/WEB-INF/typePaiement.jsp").forward(request, response);
+        } else {
+            invalidParameters(request, response);
+        }
+
+    }
+
+    /**
+     * Actions possibles en POST
+     */
+    public void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+
+        if (true) {
+
+        } else {
+            invalidParameters(request, response);
+        }
+    }
+    }
+
+ 
